e0d5f83ade4740554c993f325952862855c57e8f
Caroline Evans <carolineevans261@gmail.com>
2023-03-23 18:07:43 +0000

    added save_students method

--- directory.rb
+++ directory.rb
@@ -50,6 +50,7 @@ def print_header
   puts "-------------"
 end
 
+
 def print_students_list
   # print list of all students
   puts "Students by index number: \n "
@@ -73,6 +74,7 @@ def print_students_list
   }
 end
 
+
 def print_footer
   @students.count < 2 ? student_plural = "student" : student_plural = "students"
   string = "Overall, we have #{@students.count} great #{student_plural}"
@@ -80,12 +82,15 @@ def print_footer
   puts ""
 end
 
+
 def print_menu
   puts "\nMAIN MENU"
   puts "1. Input the students"
   puts "2. Show the students"
+  puts "3. Save the list to students.csv"
   puts "9. Exit\n "
 end
+
 
 def show_students
   print_header
@@ -93,12 +98,15 @@ def show_students
   print_footer
 end
 
+
 def process(selection)
   case selection
     when "1"
       input_students
     when "2"
       show_students
+    when "3"
+      save_students
     when "9"
       exit
     else
@@ -106,6 +114,17 @@ def process(selection)
   end
 end
 
+def save_students
+  # open the file for writing
+  file = File.open("students.csv", "w")
+  # iterate over the array of students
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
+end
 
 def interactive_menu
   loop do
